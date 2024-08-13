464840427fe34aaabaa6600819aab33941c09ad2
Georgiana <fil.georgiana03@gmail.com>
2024-08-13 23:23:02 +0300

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,24 +1,23 @@
-import { galleryItems } from './gallery-items.js'; 
-import SimpleLightbox from 'simple-lightbox';
-import 'simple-lightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector('.gallery');
 
 const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
-    return `
+    return 
         <li class="gallery__item">
             <a class="gallery__link" href="${original}">
                 <img class="gallery__image" src="${preview}" alt="${description}" />
             </a>
         </li>
-    `;
+    ;
 }).join('');
 
 gallery.innerHTML = galleryMarkup;
 
-const lightbox = new SimpleLightbox('.gallery a', {
-    captions: true,
-    captionsData: 'alt',
-    captionDelay: 250,
-    nav: true 
-});
+new SimpleLightbox('.gallery__item a',
+    {
+        captions: true,
+        captionsData: 'alt',
+        captionDelay: 250,
+        nav: true 
+    });
