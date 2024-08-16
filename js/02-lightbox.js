af8b4c9fa5ad6888a67e263f11daa4efaa0e4b6c
Georgiana <fil.georgiana03@gmail.com>
2024-08-16 22:31:08 +0300

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,21 +3,20 @@ import { galleryItems } from './gallery-items.js';
 const gallery = document.querySelector('.gallery');
 
 const galleryMarkup = galleryItems.map(({ preview, original, description }) => {
-    return 
-        <li class="gallery__item">
-            <a class="gallery__link" href="${original}">
-                <img class="gallery__image" src="${preview}" alt="${description}" />
+    return `
+        <li class="galleryitem">
+            <a class="gallerylink" href="${original}">
+                <img class="galleryimage" src="${preview}" alt="${description}" />
             </a>
         </li>
-    ;
+    `;
 }).join('');
 
 gallery.innerHTML = galleryMarkup;
 
-new SimpleLightbox('.gallery__item a',
-    {
-        captions: true,
-        captionsData: 'alt',
-        captionDelay: 250,
-        nav: true 
-    });
+new SimpleLightbox('.galleryitem a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+    nav: true 
+});
